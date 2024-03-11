bcc83cd3c136c13eca425102f8b8821dcb1648fb
Satyam Singh <96168882+satyam2209@users.noreply.github.com>
2024-03-11 11:49:14 +0000

    project completed

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,8 +1,9 @@
 import React from "react";
-import appwriteService from "../appwrite/config";
+import { Service } from "../appwrite/config";
 import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage }) {
+  const appwriteService = new Service();
   return (
     <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4">
